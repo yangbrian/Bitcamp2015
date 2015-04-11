7308a9ea0ef3dea5b98903cf378e6188b95738a5
Brian <brichen95@gmail.com>
2015-04-11 06:47:08 -0400

    Rudimentary stock search off yahoo API

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>PHP Starter Application</title>
+	<title>Sallie Bae - Bitcamp 2015</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<link rel="stylesheet" href="style.css" />
 </head>
